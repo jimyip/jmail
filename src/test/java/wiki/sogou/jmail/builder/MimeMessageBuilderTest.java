73786d1544d902df10ccfdbb51c9349ba43be567
JimYip <562612654@qq.com>
2022-04-27 23:46:37 +0800

    remove unless test case

--- src/test/java/wiki/sogou/jmail/builder/MimeMessageBuilderTest.java
+++ src/test/java/wiki/sogou/jmail/builder/MimeMessageBuilderTest.java
@@ -1,10 +1,33 @@
 package wiki.sogou.jmail.builder;
 
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
-/**
- * @author JimYip
- */
-public class MimeMessageBuilderTest {
+import javax.mail.Address;
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+
+class MimeMessageBuilderTest {
+
+    @Spy
+    private MimeMessageBuilder builder;
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testBuilder() throws MessagingException {
+        builder.from("admin@test.com");
+        MimeMessage message = builder.build();
+        Address[] from = {new InternetAddress("admin@test.com")};
+        Assertions.assertArrayEquals(message.getFrom(), from);
+    }
 
 }
